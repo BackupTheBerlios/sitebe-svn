92c5bb4894bf6061c0721b50856e23c6b24751cd
mastasushi <mastasushi@d58d75f2-7422-0410-ab8a-b5e51e13b638>
2006-11-18 14:25:00 +0000

    Mise à jour pour correspondre à la base l3isb3

--- trunk/projet-be/admin/Includes/settings.inc.php
+++ trunk/projet-be/admin/Includes/settings.inc.php
@@ -1,25 +1,17 @@
-<?php
-/*
-** Fichier : settings.inc
-** Date de creation : 08/06/2005 gerge
-** Auteurs : Conde Mickael, Badaoui Kassem, Canaye Kurvin, Guenatri Kamil
-** Version : 1.0
-** Description : Contient toutes les variables de configuration du site
-*/
-
-
-
-
-// database settings
-$db['name'] = "BE" ;
-$db['host'] = "localhost" ;
-$db['user'] = "root" ;
-$db['pass'] = "" ; 
-
-
-
-
-/*
-** EOF settings.inc
-*/
-?>
+<?php
+/*
+** Fichier : settings.inc
+** Date de creation : 08/06/2005 gerge
+** Auteurs : Conde Mickael, Badaoui Kassem, Canaye Kurvin, Guenatri Kamil
+** Version : 1.0
+** Description : Contient toutes les variables de configuration du site
+*/
+// database settings
+$db['name'] = "l3isb3" ;
+$db['host'] = "localhost" ;
+$db['user'] = "root" ;
+$db['pass'] = "" ; 
+/*
+** EOF settings.inc
+*/
+?>
