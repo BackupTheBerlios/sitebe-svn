dd61d1559fdfe47a3507188eeb98fd113937b5a3
ezeta <ezeta@d58d75f2-7422-0410-ab8a-b5e51e13b638>
2006-12-08 14:27:56 +0000

    suppression de parties inutiles de la partie admin (nature, evaluation, contrôle)

--- trunk/projet-be/admin/admin.php
+++ trunk/projet-be/admin/admin.php
@@ -216,24 +216,12 @@ if (isset($_GET['w']))
 			print("<td align=\"center\" width=\"150\"><a href=\"admin.php?w=matieres&a=mod\"><img src=\"Gfx/admin_icon_modify.gif\"></a></td>") ;	
 			print("<td align=\"center\" width=\"150\"><a href=\"admin.php?w=matieres&a=del\"><img src=\"Gfx/admin_icon_delete.gif\"></a></td>\n") ;
 			print("\t\t\t\t</tr>\n") ;
-			print("\t\t\t\t<tr>\n") ;
+			/*print("\t\t\t\t<tr>\n") ;
 			print("\t\t\t\t\t<td align=\"left\" width=\"200\"> &raquo; <b>Evaluations</b></td>") ;
 			print("<td align=\"center\" width=\"150\"><a href=\"admin.php?w=evaluation_matieres&a=add\"><img src=\"Gfx/admin_icon_add.gif\"></a></td>") ;
 			print("<td align=\"center\" width=\"150\"><a href=\"admin.php?w=evaluation_matieres&a=mod\"><img src=\"Gfx/admin_icon_modify.gif\"></a></td>") ;	
 			print("<td align=\"center\" width=\"150\"><a href=\"admin.php?w=evaluation_matieres&a=del\"><img src=\"Gfx/admin_icon_delete.gif\"></a></td>\n") ;
-			print("\t\t\t\t</tr>\n") ;
-			print("\t\t\t\t<tr>\n") ;
-			print("\t\t\t\t\t<td align=\"left\" width=\"200\"> &raquo; <b>Natures</b></td>") ;
-			print("<td align=\"center\" width=\"150\"><a href=\"admin.php?w=natures&a=add\"><img src=\"Gfx/admin_icon_add.gif\"></a></td>") ;
-			print("<td align=\"center\" width=\"150\"><a href=\"admin.php?w=natures&a=mod\"><img src=\"Gfx/admin_icon_modify.gif\"></a></td>") ;
-			print("<td align=\"center\" width=\"150\"><a href=\"admin.php?w=natures&a=del\"><img src=\"Gfx/admin_icon_delete.gif\"></a></td>\n") ;
-			print("\t\t\t\t</tr>\n") ;
-			print("\t\t\t\t<tr>\n") ;
-			print("\t\t\t\t\t<td align=\"left\" width=\"200\"> &raquo; <b>Contr&ocirc;les</b></td>") ;
-			print("<td align=\"center\" width=\"150\"><a href=\"admin.php?w=controles&a=add\"><img src=\"Gfx/admin_icon_add.gif\"></a></td>") ;
-			print("<td align=\"center\" width=\"150\"><a href=\"admin.php?w=controles&a=mod\"><img src=\"Gfx/admin_icon_modify.gif\"></a></td>") ;		
-			print("<td align=\"center\" width=\"150\"><a href=\"admin.php?w=controles&a=del\"><img src=\"Gfx/admin_icon_delete.gif\"></a></td>\n") ;
-			print("\t\t\t\t</tr>\n") ;
+			print("\t\t\t\t</tr>\n") ;*/
 			print("\t\t\t\t<tr>\n") ;
 			print("\t\t\t\t\t<td align=\"left\" width=\"200\"> &raquo; <b>Enseignements</b></td>") ;
 			print("<td align=\"center\" width=\"150\"><a href=\"admin.php?w=enseignements&a=add\"><img src=\"Gfx/admin_icon_add.gif\"></a></td>") ;
